13b8e0a9661f272c53e7b210ad49bef1517b9d74
Jonathan Sharman <jon.w.sharman@gmail.com>
2023-03-28 11:29:24 -0700

    chore: create base github action for xlog comparison

---
+++ transition-scripts/xlog-comparison.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+
+usage() {
+    cat <<EOF
+Compare the xlogs between patroni-gold and patroni-dr.  Alert team if they do not match.
+
+Steps:
+    1. Compare xlogs for different namespaces.
+    2. If the job fails or xlog comparison is off, send rocketchat alert
+
+Usages:
+    $0
+
+Available namespaces:
+    - c6af30-dev
+    - eb75ad-dev
+    - eb75ad-test
+    - eb75ad-prod
+
+EOF
+}
+
+pwd="$(dirname "$0")"
+source "$pwd/helpers/_all.sh"
+
+synch_status=$(compare_patroni_xlog "c6af30-dev")
+
+echo "The xlogs are $synch_status"
+
+if [ "$synch_status" != "synced" ]; then
+    error "the patroni clusters are not synched: ($synch_status)"
+    exit 1
+fi
