9ae49b722c72266dc81f6ea94d24235b9e7eeea2
Jonathan Sharman <jon.w.sharman@gmail.com>
2023-06-28 20:22:42 -0700

    chore: clean up the dns function, add tests

--- src/clients/dns.py
+++ src/clients/dns.py
@@ -43,7 +43,7 @@ async def dns_lookup(domain_name: str, q: Queue):
             logger.error("No DNS response")
             result = 'error'
 
-        maintenance_mode = is_keycloak_dr_up_and_receiving_traffic(ip, last_result)
+        maintenance_mode = is_keycloak_dr_up_and_receiving_traffic(result)
 
         if last_result != result:
             q.put({'event': 'dns', 'result': result,
@@ -59,15 +59,13 @@ async def dns_lookup(domain_name: str, q: Queue):
         time.sleep(5)
 
 
-def is_keycloak_dr_up_and_receiving_traffic(ip: str, last_result: str):
+def is_keycloak_dr_up_and_receiving_traffic(ip: str):
     # If the maintenance page is detected as down, but traffic is still going to the GoldDR cluster
     # It means keycloak DR must be active since traffic only goes to DR if
     # the GSLB health check is passing.
-
     if (ip == config.get('passive_ip')):
         try:
             html = urlopen("http://sso-keycloak-maintenance:8080", timeout=0.5)
-            # curl -o /dev/null -s -w "%{http_code}\n" http://sso-keycloak-maintenance:8080
         except HTTPError as e:
             logger.debug("HTTP error", e)
             return 'error'
@@ -77,17 +75,7 @@ def is_keycloak_dr_up_and_receiving_traffic(ip: str, last_result: str):
         else:
             logger.debug('Maintenance Service Up')
             return 'maintenance_up'
-        # Add logic to handle first case
     elif (ip == config.get('active_ip')):
         return 'gold_up'
     else:
         return 'error'
-
-# def send_clients_message_dr_in_progress():
-#     return False
-
-# def send_clients_message_dr_active_access_restored():
-#     return False
-
-# def send_clients_message_dr_resolved_systems_normal(ip: str, prev_ip: str):
-#     return False
