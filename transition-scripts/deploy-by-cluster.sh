67ed08c8bc79e76ccce242f7ad9040ee47464c0e
Jonathan Sharman <jon.w.sharman@gmail.com>
2024-10-17 08:55:40 -0700

    feat: create staggered deployment

---
+++ transition-scripts/deploy-by-cluster.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+set -e
+
+usage() {
+    cat <<EOF
+Deploy Keycloak resources in the target namespaces in a normal situation;
+it sets "active" mode in Gold cluster and "standby" mode in Golddr cluster.
+
+Usages:
+    $0 <namespace>
+
+Available namespaces:
+    - e4ca1d-dev
+    - e4ca1d-test
+    - e4ca1d-prod
+    - eb75ad-dev
+    - eb75ad-test
+    - eb75ad-prod
+
+Examples:
+    $ $0 e4ca1d-dev
+EOF
+}
+
+if [ "$#" -lt 1 ]; then
+    usage
+    exit 1
+fi
+
+namespace=$1
+purge="false"
+
+pwd="$(dirname "$0")"
+source "$pwd/helpers/_all.sh"
+
+while [[ "$2" =~ ^- && ! "$2" == "--" ]]; do
+    case $2 in
+    -p | --purge)
+        purge="true"
+        ;;
+    esac
+    shift
+done
+
+# Gold deployments
+switch_kube_context "gold" "$namespace"
+check_ocp_cluster "gold"
+
+helm_released=$(check_helm_release "$namespace" "sso-keycloak")
+if [ "$helm_released" == "found" ]; then
+    if [ "$purge" == "true" ]; then
+        uninstall_helm "$namespace"
+        cleanup_namespace "$namespace"
+    else
+        set_patroni_cluster_active "$namespace"
+    fi
+fi
+
+upgrade_helm_active "$namespace"
+
+wait_for_patroni_healthy "$namespace"
+wait_for_patroni_all_ready "$namespace"
+
+# Golddr deployments
+switch_kube_context "golddr" "$namespace"
+check_ocp_cluster "golddr"
+
+helm_released=$(check_helm_release "$namespace" "sso-keycloak")
+if [ "$helm_released" == "found" ]; then
+    if [ "$purge" == "true" ]; then
+        uninstall_helm "$namespace"
+        cleanup_namespace "$namespace"
+    else
+        set_patroni_cluster_standby "$namespace"
+    fi
+fi
+
+upgrade_helm_standby "$namespace" --maintenance-on
+
+wait_for_patroni_healthy "$namespace"
+wait_for_patroni_all_ready "$namespace"
+wait_for_patroni_xlog_synced "$namespace"
