3349bf2dc974ca7b9cfe76e47ab1880987b41316
Jonathan Sharman <jon.w.sharman@gmail.com>
2025-01-16 21:39:15 -0800

    chore: remove max surge and update the helm chart

--- transition-scripts/helpers/helm.sh
+++ transition-scripts/helpers/helm.sh
@@ -4,7 +4,7 @@ this="${BASH_SOURCE[0]}"
 pwd=$(dirname "$this")
 values="$pwd/../values"
 
-KEYCLOAK_HELM_CHART_VERSION="v1.16.3"
+KEYCLOAK_HELM_CHART_VERSION="v1.16.5"
 KEYCLOAK_HELM_DEPLOYMENT_NAME="sso-keycloak"
 
 upgrade_helm() {
