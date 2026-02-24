e6dcc1b202fb4413944e98c39bb74a01ff98fd04
Jonathan Sharman <jon.w.sharman@gmail.com>
2026-02-24 10:20:09 -0800

    chore: hc version

--- transition-scripts/helpers/helm.sh
+++ transition-scripts/helpers/helm.sh
@@ -4,7 +4,7 @@ this="${BASH_SOURCE[0]}"
 pwd=$(dirname "$this")
 values="$pwd/../values"
 
-KEYCLOAK_HELM_CHART_VERSION="v1.17.3"
+KEYCLOAK_HELM_CHART_VERSION="v1.17.4"
 KEYCLOAK_HELM_DEPLOYMENT_NAME="sso-keycloak"
 
 upgrade_helm() {
